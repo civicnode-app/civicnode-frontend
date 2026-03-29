003b084138cb1f666422af84b44e9b445d4c888c
Deearss <haidiraditya16@gmail.com>
2026-03-30 07:05:34 +0800

    fix(config): tambahkan konfigurasi images remotePatterns untuk domain images.unsplash.com

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,12 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 
   // Compiler
